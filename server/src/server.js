ed92ec946ac1930cbb5b3e491f83c426da0639f1
sarah-gaskov <sgaskov@csuchico.edu>
2026-03-06 15:34:33 -0800

    Changed order of imports

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const pool = require('./config/db');
-require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8080;
