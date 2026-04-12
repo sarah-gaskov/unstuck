54b65a669d490b7b389533fc61f257207b87ad2e
sarah-gaskov <sgaskov@csuchico.edu>
2026-04-11 22:22:55 -0700

    Added bcrypt to server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const pool = require('./config/db');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = process.env.PORT || 8080;
