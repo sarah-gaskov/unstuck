f4c370893f15e865211cd8259baf2ce090399637
sarah-gaskov <sgaskov@csuchico.edu>
2026-04-12 00:06:06 -0700

    Add registration functionality to api...

--- app/api_service.dart
+++ app/api_service.dart
@@ -2,34 +2,47 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  //main server URL on Fly.io
-  final String baseUrl = 'https://unstuck.fly.dev/api';
+	//main server URL on Fly.io
+	final String baseUrl = 'https://unstuck.fly.dev/api';
 
-  //Check if server is running
-  Future<bool> checkServer() async {
-    try {
-      final response = await http.get(Uri.parse('$baseUrl/'));
-      return response.statusCode == 200;
-    } catch (e) {
-      return false;
-    }
-  }
-  
-  // == Login ==
-  
-  Future<bool> loginUser(String username, String password) async {
-	try {
-		final response = await http.post(
-			Uri.parse('$baseUrl/login'),
-			
-			headers: {'Content-Type': 'application/json'},
-			body: jsonEncode({
-				'username': username,
-				'password': password,
-			}),
-		);
+	//Check if server is running
+	Future<bool> checkServer() async {
+		try {
+			final response = await http.get(Uri.parse('$baseUrl/'));
+			return response.statusCode == 200;
+		} catch (e) {
+			return false;
+		}
 	}
-  }
+  
+	// == Login ==
+  
+	Future<bool> registerUser(String username, String password) async {
+		try {
+			final response = await http.post(
+				Uri.parse('$baseUrl/register'),
+				headers: {'Content-Type': 'application/json'},
+				body: jsonEncode({
+					'username': username,
+					'password': password,
+				}),
+			);
+		}
+	}
+  
+	Future<bool> loginUser(String username, String password) async {
+		try {
+			final response = await http.post(
+				Uri.parse('$baseUrl/login'),
+				
+				headers: {'Content-Type': 'application/json'},
+				body: jsonEncode({
+					'username': username,
+					'password': password,
+				}),
+			);
+		}
+	}
   
   // == Query and Answer ==
 
